e36563f822f7c9e7bb54765edc06f6ae723a9f4d
maeEsp <maemae.espera@gmail.com>
2025-01-29 17:32:40 +0800

    working login

--- backend/pharmacy/urls.py
+++ backend/pharmacy/urls.py
@@ -1,11 +1,15 @@
 # urls.py
 from django.urls import path
-
-from .views import PersonList, Supplier, UserList, UserRole
+from .views import UserList, UserLoginView, PersonList, Supplier, UserList, UserRole
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('persons/', PersonList.as_view(), name='person-list'),
     path('users/', UserList.as_view(), name='user-list'),
     path('roles/', UserRole.as_view(), name='role-list'),
     path('suppliers/', Supplier.as_view(), name='supplier-list'),
+
+    #login
+    path("login/", UserLoginView.as_view(), name="login"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
