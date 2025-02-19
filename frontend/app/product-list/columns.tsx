514946a4e3cf85bc082851376e9007f197b54379
maeEsp <maemae.espera@gmail.com>
2025-02-19 17:29:44 +0800

    still in process edit button

--- frontend/app/product-list/columns.tsx
+++ frontend/app/product-list/columns.tsx
@@ -6,6 +6,16 @@ import { ArrowUpDown } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
 import { Checkbox } from "@/components/ui/checkbox";
+import { useState } from "react";
+import {
+  Dialog,
+  DialogHeader,
+  DialogTitle,
+  DialogContent,
+  DialogTrigger,
+  DialogDescription,
+} from "@/components/ui/dialog";
+import EditProductForm from "@/components/forms/EditProductForm";
 interface Products {
   product_id: number;
   full_product_name: string;
@@ -15,7 +25,48 @@ interface Products {
   unit: string;
 }
 
-export const  columns: (onSuccess: () => void) => ColumnDef<Products>[] = (onSuccess) => [
+interface EditProductDialogProps {
+  product_id: number;
+  onSuccess: () => void;
+}
+
+const EditProductDialog = ({
+  product_id,
+  onSuccess,
+}: EditProductDialogProps) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+          <Button>Edit</Button>
+        </DialogTrigger>
+
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Edit Product Details</DialogTitle>
+            <DialogDescription>
+              Update the product&apos;s information
+            </DialogDescription>
+            <EditProductForm
+              product_id={product_id}
+              onSuccess={(data) => {
+                setOpen(false);
+                onSuccess();
+                console.log("From the columns component", data);
+              }}
+            ></EditProductForm>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+};
+
+export const columns: (onSuccess: () => void) => ColumnDef<Products>[] = (
+  onSuccess
+) => [
   {
     id: "select",
     header: ({ table }) => (
@@ -60,8 +111,14 @@ export const  columns: (onSuccess: () => void) => ColumnDef<Products>[] = (onSuc
   },
   {
     id: "actions",
-    cell: ({  }) => {
-      return <></>
+    cell: ({ row }) => {
+      const product = row.original;
+      // console.log(product.product_id)
+
+      return <EditProductDialog product_id={product.product_id} onSuccess={(data) => {
+        console.log("Data:", data)
+        onSuccess()
+      }}></EditProductDialog>
     },
-  }
+  },
 ];
