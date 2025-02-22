5123ecfe655e49d41a65b71ba8e8aa53a744db7a
maeEsp <maemae.espera@gmail.com>
2025-02-22 22:53:32 +0800

    changed styling for navbar

--- frontend/components/NavBar.tsx
+++ frontend/components/NavBar.tsx
@@ -7,6 +7,11 @@ import {
 } from "@/components/ui/navigation-menu";
 import Link from "next/link";
 import Image from "next/image";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "./ui/collapsible";
 
 export default function NavBar() {
   return (
@@ -20,26 +25,40 @@ export default function NavBar() {
             height={100}
             unoptimized
           />
-          <h1 className="mt-4 text-lg font-semibold text-[#303086]">Amry Pharmacy</h1>
+          <h1 className="mt-4 text-lg font-semibold text-[#303086]">
+            Amry Pharmacy
+          </h1>
         </div>
-
         <NavigationMenu>
-          <NavigationMenuList className="flex flex-col p-4 space-y-2 items-start ">
-            <NavigationMenuItem className="">
+          <NavigationMenuList className="flex flex-col p-4 space-y-2 items-stretch ">
+            <NavigationMenuItem>
               <Link href="/dashboard" legacyBehavior passHref>
                 <NavigationMenuLink className={navigationMenuTriggerStyle()}>
                   Dashboard
                 </NavigationMenuLink>
               </Link>
             </NavigationMenuItem>
+            <Collapsible>
+              <CollapsibleTrigger className="group inline-flex h-9 w-max items-center justify-center rounded-md text-[#212529] px-4 py-2 text-sm transition-colors hover:bg-[#DEE0F3] hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50">Inventory
+              </CollapsibleTrigger>
+              <CollapsibleContent className="pl-4">
+              <NavigationMenuItem>
+              <Link href="/product-list" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Products
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
             <NavigationMenuItem>
-              
               <Link href="/inventory" legacyBehavior passHref>
                 <NavigationMenuLink className={navigationMenuTriggerStyle()}>
                   Inventory
                 </NavigationMenuLink>
               </Link>
             </NavigationMenuItem>
+              </CollapsibleContent>
+            </Collapsible>
+            
             <NavigationMenuItem>
               <Link href="#" legacyBehavior passHref>
                 <NavigationMenuLink className={navigationMenuTriggerStyle()}>
