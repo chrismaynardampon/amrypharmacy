95efbf6c146ba05a20ce59aaa4db161964bd6695
maeEsp <maemae.espera@gmail.com>
2025-03-23 11:09:01 +0800

    edited navbar

--- frontend/components/NavBar.tsx
+++ frontend/components/NavBar.tsx
@@ -56,31 +56,14 @@ export default function NavBar() {
                 </NavigationMenuLink>
               </Link>
             </NavigationMenuItem>
-            <Collapsible>
-              <CollapsibleTrigger className="group inline-flex h-9 w-max items-center justify-center rounded-md text-[#212529] px-4 py-2 text-sm transition-colors hover:bg-[#DEE0F3] hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50">
-                Inventory
-              </CollapsibleTrigger>
-              <CollapsibleContent className="pl-4">
-                <NavigationMenuItem>
-                  <Link href="/product-list" legacyBehavior passHref>
-                    <NavigationMenuLink
-                      className={navigationMenuTriggerStyle()}
-                    >
-                      Products
-                    </NavigationMenuLink>
-                  </Link>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                  <Link href="#" legacyBehavior passHref>
-                    <NavigationMenuLink
-                      className={navigationMenuTriggerStyle()}
-                    >
-                      Inventory
-                    </NavigationMenuLink>
-                  </Link>
-                </NavigationMenuItem>
-              </CollapsibleContent>
-            </Collapsible>
+
+            <NavigationMenuItem>
+              <Link href="/product-list" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Inventory
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
 
             <NavigationMenuItem>
               <Link href="/pos" legacyBehavior passHref>
